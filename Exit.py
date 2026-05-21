6f3426a2f31b170e7a2d7739812fd24e50108f34
maltoresel835-lab <maltoresel835@gmail.com>
2026-05-21 21:07:33 +0800

    Create Exit.py

---
+++ Exit.py
@@ -0,0 +1,19 @@
+# =========================================
+# EXIT FUNCTION
+# =========================================
+
+def exit_program():
+
+    print("\n====================================")
+    print("   EXITING LIBRARY MANAGEMENT")
+    print("====================================")
+    print("Thank you for using the system!")
+    print("Program Ended Successfully.")
+    print("====================================")
+
+
+# =========================================
+# MAIN PROGRAM
+# =========================================
+
+exit_program()
